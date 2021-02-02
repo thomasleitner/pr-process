69d21a40af8c5ff18836f2a54416e40efe7b12a2
thomasleitner <36305256+thomasleitner@users.noreply.github.com>
2021-02-02 12:33:49 +0100

    Changed because logging1.powerrouter.com is not available anymore.

--- forward.sh
+++ forward.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 i=0
-stdbuf -i0 -o0 -e0 socat -v tcp-l:80,fork,reuseaddr tcp:logging1-orig.powerrouter.com:80 2>&1 | \
+stdbuf -i0 -o0 -e0 socat tcp-l:80,fork,reuseaddr - | \
     stdbuf -i0 -o0 -e0 fgrep '{"header"' | while read a ;
  do
     f=`printf pr%d.dat $i`
     echo $f
-    echo "$a" | sed -e 's/<.*//g' >/mytmp/$f
+    echo "$a" | sed -e 's/<.*//g' | sed -e 's/POST.*$//g' >/mytmp/$f
     i=`expr $i + 1`
 done
